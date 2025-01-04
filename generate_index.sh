3712c60bdc88e267635814d72840599f2c92ee00
rain <1637427916@qq.com>
2025-01-04 02:44:35 -0800

    change script

--- generate_index.sh
+++ generate_index.sh
@@ -12,8 +12,19 @@ echo "<body>" >> index.html
 echo "    <h1>Index of Markdown and Text Files</h1>" >> index.html
 echo "    <ul>" >> index.html
 
-# Find all .md and .txt files and add them to the index.html file
-find . -type f \( -name "*.md" -o -name "*.txt" \) | while read -r file; do
+# Find all .md files, convert them to .html, and add them to the index.html file
+find . -type f -name "*.md" | while read -r file; do
+    # Convert .md file to .html using pandoc
+    html_file="${file%.md}.html"
+    /usr/bin/pandoc "$file" -o "$html_file"
+    
+    # Add the .html file link to index.html
+    filename=$(basename "$html_file")
+    echo "        <li><a href=\"$html_file\">$filename</a></li>" >> index.html
+done
+
+# Find all .txt files and add them to the index.html file
+find . -type f -name "*.txt" | while read -r file; do
     filename=$(basename "$file")
     echo "        <li><a href=\"$file\">$filename</a></li>" >> index.html
 done
